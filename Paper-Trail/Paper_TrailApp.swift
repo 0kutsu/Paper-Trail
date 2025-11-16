2a3a9079c7dc1d1be3b332f9095d547c66661f49
Mati Okutsu <mati.okutsu@gmail.com>
2025-11-15 19:51:25 -0500

    Initial Commit

---
+++ Paper-Trail/Paper_TrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  Paper_TrailApp.swift
+//  Paper-Trail
+//
+//  Created by Mati Okutsu on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Paper_TrailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
